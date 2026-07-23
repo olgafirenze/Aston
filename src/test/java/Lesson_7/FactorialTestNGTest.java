2ae1274f7cb89dadbcccaa5faf79bacc766182cc
olgafirenze <olgafirenze@gmail.com>
2026-07-23 14:10:32 +0300

    новая ветка

---
+++ src/test/java/Lesson_7/FactorialTestNGTest.java
@@ -0,0 +1,26 @@
+package Lesson_7;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FactorialTestNGTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testFactorial() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+}
