087898b16d2408347cbcd222025f1b74ab361d77
olgafirenze <olgafirenze@gmail.com>
2026-07-24 14:16:05 +0300

    FactorialTestNGTest

--- src/test/java/Lesson_7/FactorialTestNGTest.java
+++ src/test/java/Lesson_7/FactorialTestNGTest.java
@@ -1,26 +1,30 @@
 package Lesson_7;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
+import org.testng.annotations.*;
 import static org.testng.Assert.*;
+
+import java.math.BigInteger;
+
+import static Lesson_7.Factorial.factorial;
+
 
 public class FactorialTestNGTest {
 
-    @BeforeMethod
-    public void setUp() {
+    @Test(dataProvider = "validData")
+    public Object[][] validData() {
+        return new Object[][] {
+                {0, 1},
+                {1, 1},
+                {4, 24},
+                {5, 120},
+                {9, 362880},
+                {25, 15511210043330985984000000}
+        };
     }
 
-    @AfterMethod
-    public void tearDown() {
+    @Test(dataProvider = "validData")
+    public void factorial_shouldReturnCorrectResult(int n, BigInteger expected) {
+        assertEquals(factorial(n), BigInteger.valueOf(expected));
     }
 
-    @Test
-    public void testFactorial() {
-    }
-
-    @Test
-    public void testMain() {
-    }
 }
