1473d26bf938b284f53f35e81b3ee1ac5a2ac3a6
olgafirenze <olgafirenze@gmail.com>
2026-07-24 15:04:33 +0300

    FactorialTestNGTest update + pom update

--- src/test/java/Lesson_7/FactorialTestNGTest.java
+++ src/test/java/Lesson_7/FactorialTestNGTest.java
@@ -10,21 +10,29 @@ import static Lesson_7.Factorial.factorial;
 
 public class FactorialTestNGTest {
 
-    @Test(dataProvider = "validData")
+    @DataProvider ( name = "validData")
     public Object[][] validData() {
         return new Object[][] {
-                {0, 1},
-                {1, 1},
-                {4, 24},
-                {5, 120},
-                {9, 362880},
-                {25, 15511210043330985984000000}
+                {0, BigInteger.ONE},
+                {1, BigInteger.ONE},
+                {4, BigInteger.valueOf(24)},
+                {5, BigInteger.valueOf(120)},
+                {9, BigInteger.valueOf(362880)},
+                {25, new BigInteger("15511210043330985984000000")}
         };
     }
 
-    @Test(dataProvider = "validData")
+    @Test(dataProvider = "validData", description = "Возвращает верный результат для корректных данных")
     public void factorial_shouldReturnCorrectResult(int n, BigInteger expected) {
-        assertEquals(factorial(n), BigInteger.valueOf(expected));
+        assertEquals(factorial(n), expected);
+    }
+
+
+
+    @Test(expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Число должно быть >= 0")
+    public void factorial_shouldThrowException() {
+        factorial(-7);
     }
 
 }
