8770e6670471e8d829746acbb9a8822b87ba21dc
olgafirenze <olgafirenze@gmail.com>
2026-07-27 19:51:47 +0300

    pom update

---
+++ src/main/java/Lesson_8/ApiEchoTests.java
@@ -0,0 +1,11 @@
+package Lesson_8;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.*;
+
+public class ApiEchoTests {
+}
