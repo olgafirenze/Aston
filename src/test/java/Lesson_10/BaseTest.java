00320beadcd548be213e35dd0273058407981cd2
olgafirenze <olgafirenze@gmail.com>
2026-07-30 17:26:50 +0300

    ДЗ 9

--- src/test/java/Lesson_10/BaseTest.java
+++ src/test/java/Lesson_10/BaseTest.java
@@ -1,4 +1,4 @@
-package Lesson_9;
+package Lesson_10;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/Lesson_10/MtsOnlinePaymentTest.java
+++ src/test/java/Lesson_10/MtsOnlinePaymentTest.java
@@ -1,4 +1,4 @@
-package Lesson_9;
+package Lesson_10;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
